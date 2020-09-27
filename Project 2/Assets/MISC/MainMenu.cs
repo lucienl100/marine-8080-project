60fda2fb4af9537c3e45cec59edd131dc24e645f
lucienl100 <lucienl100@users.noreply.github.com>
2020-09-27 22:45:44 +1000

    Added Main Menu

---
+++ Project 2/Assets/MISC/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas mm;
+    public Canvas set;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level01", LoadSceneMode.Additive);
+    }
+    public void Settings()
+    {
+        mm.enabled = false;
+        set.enabled = true;
+    }
+}
