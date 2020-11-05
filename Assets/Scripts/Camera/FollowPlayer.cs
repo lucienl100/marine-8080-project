a7ddf827be076c35e9b66c7cdc5977ed156cc15c
lucienl100 <lucienl100@users.noreply.github.com>
2020-11-06 02:54:44 +1100

    Update FollowPlayer

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -7,6 +7,7 @@ public class FollowPlayer : MonoBehaviour
     public Transform player;
     private Transform cam;
     public float speed = 3f;
+    public float camDepth = -17.5f
     void Start()
     {
         cam = this.transform;
@@ -14,7 +15,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveposition = new Vector3(player.position.x, player.position.y, cam.position.z);
+        Vector3 moveposition = new Vector3(player.position.x, player.position.y, camDepth);
         cam.position = Vector3.Slerp(cam.position, moveposition, Time.deltaTime * speed);
         cam.position = new Vector3(cam.position.x, cam.position.y, moveposition.z);
     }
